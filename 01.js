b801b4623204bd86e464379ab53c453360a33077
Lamiaa Selim <lamiaaselim1896@gmail.com>
2025-10-24 10:56:01 +0300

    edit file named 01.js add +

--- 01.js
+++ 01.js
@@ -9,10 +9,10 @@
   What Is Callback
   - A Function That Is Passed Into Another One As An Argument To Be Executed Later
   - Function To Handle Photos
-  --- [1] Download Photo From URL
-  --- [2] Resize Photo
-  --- [3] Add Logo To The Photo
-  --- [4] Show The Photo In Website
+  --- [1+] Download Photo From URL
+  --- [2+] Resize Photo
+  --- [3+] Add Logo To The Photo
+  --- [4+] Show The Photo In Website
   https://jsonplaceholder.typicode.com/todos
 */
 
